92e8a7f7e4b24219f725b3dff923abea93893022
chethan.m <chethan.m@arithaconsulting.com>
2024-02-26 14:09:52 +0530

    Chore: refactor of code

--- src/components/HeroBrikOne.jsx
+++ src/components/HeroBrikOne.jsx
@@ -1,45 +1,38 @@
+/* eslint-disable react/prop-types */
 import React from "react";
 import { Title,Text,Button} from "@apf/core";
 import "@apf/core/dist/style.css";
 import "./HeroBrikOne.css";
 function HeroOne(props){
-    // eslint-disable-next-line react/prop-types
     const backgroundImg = props?.data?.data?.find((item) => item?.content?.type === "img" && item?.content?.for === "background");
-    // eslint-disable-next-line react/prop-types
     const hoverImg = props.data.data.find((item) => item.content.type === "img" && item.content.for === "image one");
-    // eslint-disable-next-line react/prop-types
     const titleObj = props.data.data.find((item) => item.content.type === "title");
-    // eslint-disable-next-line react/prop-types
     const textObj = props.data.data.find((item) => item.content.type === "text");
-    // eslint-disable-next-line react/prop-types
     const buttonObj = props.data.data.find((item) => item.content.type === "button");
-    // eslint-disable-next-line react/prop-types
-    const logo = props?.data?.data.find((item) => item?.content?.type === "img" && item?.content?.for === "logo");
+    const handleButtonClick = () => {
+        window.open(buttonObj?.content?.link, buttonObj?.content?.target);
+      };
     return(
-    <div className="Hero-Containor">
-        <div className="Hero-Image-Containor" style={backgroundImg && backgroundImg.content.src? null :  {backgroundColor: `${backgroundImg}` }}>
+    <div className="Hero-Containor" style={{ backgroundColor: props?.data?.style?.backgroundColor }}>
+        {backgroundImg && backgroundImg?.display === "block" &&<div className="Hero-Image-Containor" style={backgroundImg && backgroundImg.content.src? null :  {backgroundColor: `${backgroundImg}` }}>
                 {backgroundImg && backgroundImg.content.src &&<img src={backgroundImg.content.src} className="HeroImageDiv"></img>}
-            </div>
-            <div className="logo">
-                    <img src={logo?.content?.src} className="logoTag"></img>
-                </div>
-
+            </div>}
         <div className="Hero-content-container">
-            <div className="child-container-a">
-                
-                <div className="hero-title">
+            <div className={hoverImg?.display === "block" ? "child-container-a" : "child-container-a-1"} >               
+            {titleObj && titleObj?.display === "block" &&<div className="hero-title">
                     <Title c={titleObj?.style?.font?.color} fz={titleObj?.style?.font?.size}  style={{ fontFamily: titleObj?.style?.font?.family }} fw={titleObj?.style?.font?.weight} className="hero-title-Tag">{ titleObj.content.value}</Title>    
-                </div> 
-                <div className="hero-text">
+                </div> }
+                {textObj && textObj?.display === "block" &&<div className="hero-text">
                     <Text  c={textObj?.style?.font.color} fz={textObj?.style?.font?.size} style={{ fontFamily: textObj?.style?.font?.family }} fs={textObj?.style?.font?.style} fw={textObj?.style?.font?.weight} className="hero-text-Tag"> {textObj.content.value}</Text>
-                </div>
-                <div className="hero-button">
-                    <Button style={{ color: buttonObj.style.font.color, backgroundColor: buttonObj.style.backgroundColor ,fontFamily: buttonObj.style.font.family}}  size={buttonObj.style.font.size} variant="Default" radius={buttonObj.style.radius}  className="ChildButtonDiv">{buttonObj.content.value}</Button>
-                </div>
+                </div>}
+                {buttonObj && buttonObj?.display === "block" &&<div className="hero-button">
+                    <Button style={{ color: buttonObj.style.font.color, backgroundColor: buttonObj.style.backgroundColor ,fontFamily: buttonObj.style.font.family, height: buttonObj?.style?.height, width: buttonObj?.style?.width}}  variant="Default" radius={buttonObj.style.radius} onClick={handleButtonClick}  className="ChildButtonDiv">{buttonObj.content.value}</Button>
+                </div>}
             </div>
-            <div className="child-container-b" style={hoverImg && hoverImg.content.src? null :  null}>
+            {hoverImg && hoverImg?.display === "block" &&<div className="child-container-b" style={{
+                display: hoverImg && hoverImg.content.src ? null : null,}}>
                 {hoverImg && hoverImg.content.src &&<img src={hoverImg.content.src} className="ChildImageTag"></img>}
-            </div>
+            </div>}
         </div>
     </div>
     );
